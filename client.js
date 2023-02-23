6f2700d8ae55175595771d90f1a8c8da2d47cb0d
Gaurav Basu <gauravbasu6@gmail.com>
2023-02-23 14:42:16 +0530

    first commit to basu branch

--- client.js
+++ client.js
@@ -6,48 +6,154 @@ const fs = require('fs');
 const SERVER = process.env.SERVER;
 const PORT = process.env.PORT || 3000;
 
-let storageSpaceState = 'LOW';
-const THRESHOLD0 = 0.4;
-const THRESHOLD1 = 0.8;
+let storageSpaceState_mem = 'LOW';
+let storageSpaceState_cpu = 'LOW';
+let numberOfTasks = Array(3).fill(0);
+let timeOfTasks = Array(3).fill(100000);
+const THRESHOLD0_mem = 0.4;
+const THRESHOLD1_mem = 0.8;
+const THRESHOLD0_cpu = 0.2;//added cpu lower threshold
+const THRESHOLD1_cpu = 0.9;//added cpu higher threshold
+
+const task_data = () =>{
+    const randnum = Math.floor(Math.random()*2); //for testing purposes, in deployment this variable is user defined where he tells if it is data or a task
+    return randnum;// 0 for task 1 for data
+}
+
+const getNfTf = () =>{
+    const randnumTf = Math.floor(Math.random()*10);
+    const randnumNf = Math.random()*5;
+    return{
+            NF: randnumNf,
+            TF: randnumTf
+    };
+}
+
+const taskParameters = (NF,TF) =>{
+    timeOfTasks[0] = timeOfTasks[1];
+    numberOfTasks[0] = numberOfTasks[1];
+    timeOfTasks[1] = timeOfTasks[2];
+    numberOfTasks[1] = numberOfTasks[2];
+    timeOfTasks[2] = NF;
+    numberOfTasks[2] = TF;
+
+}
+
+
 const getRatio = diskSpace => {
-    // return { ratio: diskSpace.free / diskSpace.size }; // use this for production
-    const randNum = Math.random(); // for testing purposes
-    return { ratio: randNum }; // for testing purposes
-};
+    // return { ratio_mem: diskSpace.free / diskSpace.size }; // use this for production
+    const randNum_mem = Math.random(); // for testing purposes
+    const randNum_cpu = (Math.random()*0.9);
+    return { ratio_mem: randNum_mem,
+            ratio_cpu: randNum_cpu }; //added a cpu load evaluator
+};// make another function that actually calculates CPU load (Med Priority)
 const sendStorageStatusUpdate = () => {
     diskUsage(process.env.DISKPATH).then(diskSpace => {
-        const ratio = getRatio(diskSpace).ratio;
+        const ratio_mem = getRatio(diskSpace).ratio_mem;
+        const ratio_cpu = getRatio(diskSpace).ratio_cpu;
+       
+
+        if (ratio_mem <= THRESHOLD0_mem) {
+            storageSpaceState_mem = 'LOW';
+        } else if (THRESHOLD0_mem < ratio_mem && ratio_mem <= THRESHOLD1_mem) {
+            storageSpaceState_mem = 'MID';
+        } else {
+            storageSpaceState_mem = 'HIG';
+        }
+
+        
+        if (ratio_cpu <= THRESHOLD0_cpu) {
+            storageSpaceState_cpu = 'LOW';
+        } else if (THRESHOLD0_cpu < ratio_cpu && ratio_cpu <= THRESHOLD1_cpu) {
+            storageSpaceState_cpu = 'MID';
+        } else {
+            storageSpaceState_cpu = 'HIG';
+        }
+
         const storageInfo = {
             ...diskSpace,
-            ratio: ratio,
-        };
-
-        if (ratio <= THRESHOLD0) {
-            storageSpaceState = 'LOW';
-            socket.emit('storage_info', {
-                ...storageInfo,
-                state: storageSpaceState,
-            });
-        } else if (THRESHOLD0 < ratio && ratio <= THRESHOLD1) {
-            storageSpaceState = 'MID';
-            socket.emit('storage_info', {
-                ...storageInfo,
-                state: storageSpaceState,
-            });
-        } else {
-            storageSpaceState = 'HIG';
-            socket.emit('storage_info', {
-                ...storageInfo,
-                state: storageSpaceState,
-            });
+            ratio_mem: ratio_mem,
+            ratio_cpu: ratio_cpu
         }
+
+        const task_or_data = task_data();
+        if(task_or_data){
+        socket.emit('storage_info', {
+            ...storageInfo,
+            state_mem: storageSpaceState_mem,
+            state_cpu: storageSpaceState_cpu,
+            NP:timeOfTasks[0],
+            NC:timeOfTasks[1],
+            NF:timeOfTasks[2],
+            TP:numberOfTasks[0],
+            TC:numberOfTasks[1],
+            TF:numberOfTasks[2]
+        })}
+
+        else{
+        //NF = getNfTf().NF;
+        //TF = getNfTf().TF;
+        //taskParameters(NF,TF)
+        socket.emit('cpu_info', {
+            ...storageInfo,
+            state_mem: storageSpaceState_mem,
+            state_cpu: storageSpaceState_cpu,
+            NP:timeOfTasks[0],
+            NC:timeOfTasks[1],
+            NF:timeOfTasks[2],
+            TP:numberOfTasks[0],
+            TC:numberOfTasks[1],
+            TF:numberOfTasks[2]
+        })}
+
+
     });
 };
+
+
+/* const sendCPUStatusUpdate = () => {
+    diskUsage(process.env.DISKPATH).then(diskSpace => {
+        const ratio_cpu = getRatio(diskSpace).ratio_cpu;
+       
+        
+        const storageInfo = {
+            ...diskSpace,
+            ratio_cpu: ratio_cpu,
+            
+        };
+
+        if (ratio_cpu <= THRESHOLD0_cpu) {
+            storageSpaceState_cpu = 'LOW';
+            socket.emit('cpu_info', {
+                ...storageInfo,
+                state_cpu: storageSpaceState_cpu,
+            });
+        } else if (THRESHOLD0_cpu < ratio_cpu && ratio_cpu <= THRESHOLD1_cpu) {
+            storageSpaceState_cpu = 'MID';
+            socket.emit('cpu_info', {
+                ...storageInfo,
+                state_cpu: storageSpaceState_cpu,
+            });
+        } else {
+            storageSpaceState_cpu = 'HIG';
+            socket.emit('cpu_info', {
+                ...storageInfo,
+                state_cpu: storageSpaceState_cpu,
+            });
+        }
+
+    });
+}; */
+
+
+
+
 
 const socket = io(SERVER + PORT);
 socket.on('connect', () => {
     console.log(socket.id);
     sendStorageStatusUpdate();
+    //sendCPUStatusUpdate();
 });
 
 socket.on('storage_info', () => {
@@ -80,6 +186,22 @@ socket.on('high_storage_send_data', () => {
     });
 });
 
+
+socket.on('high_cpu_send_task', () => {  // make sure that the file is actually being transferred (LOW priority)
+   
+    NF = getNfTf().NF;
+    TF = getNfTf().TF;
+        
+    const fileName = 'a.out';
+    socket.emit('high_cpu_send_task', {
+        fileName: fileName,
+        NF: NF,
+        TF: TF
+    });
+});
+
+
+
 socket.on('receive_data', data => {
     console.log(`saving data to file`, data.fileName);
     // fs.writeFile(
@@ -96,6 +218,14 @@ socket.on('receive_data', data => {
     // );
 });
 
+
+
+socket.on('receive_task', data => {
+    taskParameters(data.NF,data.TF);
+    console.log(`adding task for execution`, data.data);
+});
+
 setInterval(() => {
     sendStorageStatusUpdate();
+    //sendCPUStatusUpdate();
 }, process.env.DISKPOLLINTERVAL);
--- server.js
+++ server.js
@@ -6,10 +6,20 @@ const PORT = process.env.PORT || 3000;
 
 const io = new Server(PORT);
 
+const infinity = Math.pow(10,1000);
+const CPUth = 1.5;
+const w1 = 0.8;
+const w2 = 0.1;
+const w3 = 0.1;
+
 let intervals = [];
 let clients = {};
 let dataQueue = [];
-let clientReceiveCounter = 0;
+let taskQueue = [];
+let clientDataReceiveCounter = 0;
+let clientTaskRecieveCounter = 0;
+let clientTaskQueueCounter = 0;
+
 const checkLowStorageUsageClients = () => {
     let lowStorageUsageClients = [];
     for (const clientId in clients) {
@@ -19,12 +29,21 @@ const checkLowStorageUsageClients = () => {
     return lowStorageUsageClients;
 };
 
+const checkLowCPUUsageClients = () => {
+    let lowCPUUsageClients = [];
+    for (const clientId in clients) {
+        const ratio = clients[clientId].storageRatio_cpu;
+        if (ratio != null) lowCPUUsageClients.push(ratio);
+    }
+    return lowCPUUsageClients;
+};
+
 io.on('connection', socket => {
     console.log('user connected', socket.id);
     clients[socket.id] = {};
 
     socket.on('storage_info', data => {
-        if (data.state === 'HIG') {
+        if (data.state_mem === 'HIG') {
             console.log('HIG storage usage in', socket.id);
             if (checkLowStorageUsageClients().length > 0)
                 socket.emit('high_storage_send_data');
@@ -38,14 +57,62 @@ io.on('connection', socket => {
             dataQueue = dataQueue.slice(1);
         }
         clients[socket.id] = {
-            storageState: data.state,
-            storageRatio: data.ratio,
+            storageState: data.state_mem,
+            storageRatio: data.ratio_mem,
+            storageState_cpu: data.state_cpu,
+            storageRatio_cpu: data.ratio_cpu,
             socket: socket,
+            NP: data.NP,
+            NC: data.NC,
+            NF: data.NF,
+            TP: data.TP,
+            TC: data.TC,
+            TF: data.TF,
+
         };
     });
+
+
+    socket.on('cpu_info', data => {
+        if (data.state_cpu === 'HIG' || data.state_mem === 'HIG') {
+            console.log('HIG Resource usage in', socket.id);//make one function for storage_info and cpu_info (HIGH Priority)
+            if (checkLowCPUUsageClients().length > 0)
+                socket.emit('high_cpu_send_task');
+        } else if (taskQueue.length !== 0 && !(data.state_cpu=='HIG' && data.state_mem=='HIG')) {
+            const queuedTask = taskQueue[0].data;
+            const queuedFromClient = taskQueue[0].fromClient;
+            const NF = taskQueue[0].NF;
+            const TF = taskQueue[0].TF;
+            console.log(
+                `LOW/MID Resource usage in ${socket.id}, sending queued task of ${queuedFromClient}`
+            );
+            socket.emit('receive_task',{ data: queuedTask,
+                                        NF: NF,
+                                        TF: TF
+            });
+            taskQueue = taskQueue.slice(1);
+        }
+        clients[socket.id] = {
+            storageState_cpu: data.state_cpu,
+            storageRatio_cpu: data.ratio_cpu,
+            storageState: data.state_mem,
+            storageRatio: data.ratio_mem,
+            socket: socket,
+            NP: data.NP,
+            NC: data.NC,
+            NF: data.NF,
+            TP: data.TP,
+            TC: data.TC,
+            TF: data.TF,
+        };
+    });
+
+
+
+
     socket.on('high_storage_send_data', data => {
         // console.log('receive count:', ++clientReceiveCounter);
-        clientReceiveCounter++;
+        clientDataReceiveCounter++;
 
         let lowestStorageClient = null;
         let lowestStorageRatio = 1;
@@ -83,6 +150,56 @@ io.on('connection', socket => {
             clients[lowestStorageClient].socket.emit('receive_data', data);
         }
     });
+
+
+    socket.on('high_cpu_send_task', data => {
+        // console.log('receive count:', ++clientDataReceiveCounter);
+
+        let lowestcpuClient = null;
+        let lowestcpuN = infinity;
+        for (const clientId in clients) {
+            const cpuN = w1*clients[clientId].TP/clients[clientId].NP + w2*clients[clientId].TC/clients[clientId].NC + w3*clients[clientId].TP/clients[clientId].NP; 
+            if (cpuN != null && cpuN < lowestcpuN) {
+                lowestcpuN = cpuN;
+                lowestcpuClient = clientId;
+            }
+        }
+            console.log(lowestcpuN)
+        if (lowestcpuClient === null) {
+            console.log(
+                'Error: no information on client resource usage available. Adding task to data queue.'
+            );
+            taskQueue.push({
+                data: data.filename,
+                fromClient: socket.id,
+                NF: data.NF,
+                TF: data.TF
+            });
+        } else if (lowestcpuN >= CPUth) {
+            clientTaskQueueCounter++;
+            console.log(
+                'Error: no client capable of taking task. Adding task to data queue.'
+            );
+            taskQueue.push({
+                data: data.filname,
+                fromClient: socket.id,
+                NF: data.NF,
+                TF: data.TF
+            });
+        } else {
+        clientTaskRecieveCounter++;
+            console.log(
+                'Transferring task from',
+                socket.id,
+                'to',
+                lowestcpuClient
+            );
+            clients[lowestcpuClient].socket.emit('receive_task', {data: data.filename,
+                NF: data.NF,
+                TF: data.TF
+            });
+        }
+    });
 });
 
 process.on('exit', () => {
@@ -91,7 +208,9 @@ process.on('exit', () => {
 
 // testing interval for process
 setTimeout(() => {
-    console.log('receive count:', clientReceiveCounter);
+    console.log('received data count:', clientDataReceiveCounter);
+    console.log('receive tasks from other clients:',clientTaskRecieveCounter);
+    console.log('tasks added to queue:',clientTaskQueueCounter );
     io.close();
     process.exit();
 }, 60 * 1000);
