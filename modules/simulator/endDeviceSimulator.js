abf6d6046d9c9205d096adf561884752a27c4e48
Mayank Mathur <Mynk-9@users.noreply.github.com>
2023-03-30 16:14:26 +0530

    Updated endDeviceSimulator

--- modules/simulator/endDeviceSimulator.js
+++ modules/simulator/endDeviceSimulator.js
@@ -1,21 +1,25 @@
 const seedrandom = require('seedrandom');
 const rng = seedrandom();
-function getRandomInteger(min, max){
+
+const getRandomInteger = (min, max) => {
     return Math.floor(rng() * (max - min + 1)) + min;
-}
-function requestTask(){
-    let check = Array.from({length: getRandomInteger(1,10)}, () => getRandomInteger(1, 10));
+};
+
+const requestTask = () => {
+    let check = Array.from({ length: getRandomInteger(1, 10) }, () =>
+        getRandomInteger(1, 10)
+    );
     check = [...new Set(check)];
-    check.sort(function(a,b){
-        if(a.length == b.length) return (a < b ? -1 : 1);
-        return (a.lenght < b.length ? -1 : 1);
+    check.sort((a, b) => {
+        if (a.length == b.length) return a < b ? -1 : 1;
+        return a.lenght < b.length ? -1 : 1;
     });
-    const task = {
+
+    return {
         id: getRandomInteger(1000, 10000),
         name: 'dummyname',
-        swList : check
-    }
-    console.log(`Request to execute task id: ${task.id}`);
-    console.log(`The array is: ${task.swList}`)
-}
-setInterval(requestTask,getRandomInteger(2000,3000));
+        swList: check,
+    };
+};
+
+export { requestTask };
