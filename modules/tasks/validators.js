1514ffc33d43deefdd5666089134df5abeb1cd01
Mayank Mathur <Mynk-9@users.noreply.github.com>
2023-04-05 22:36:08 +0530

    added affinity check for for task transfers

--- modules/tasks/validators.js
+++ modules/tasks/validators.js
@@ -1,11 +1,12 @@
 import { DEVICE_ID } from '../cloudlet/constants.js';
 import { getNodeStatInformation } from '../information-manager/index.js';
 import {
+    AFFINITY_STATE,
     APPLICATIONS_STATE,
     CPU_STATE,
     STORAGE_STATE,
 } from '../information-manager/constants.js';
-import { HIG_STATE } from '../system-stats/constants.js';
+import { AFFINITY_THRESHOLD, HIG_STATE } from '../system-stats/constants.js';
 
 const checkSubset = (parentArray, subsetArray) => {
     return subsetArray.every(el => {
@@ -29,6 +30,10 @@ const checkTaskRunnable = task => {
             task.swList
         )
     )
+        return false;
+
+    // check if affinity is less than affinity threshold
+    if (getNodeStatInformation(DEVICE_ID, AFFINITY_STATE) >= AFFINITY_THRESHOLD)
         return false;
 
     return true;
