cf5a27f2f08bf8508f36d656bd61e4b8513d1412
Mayank Mathur <Mynk-9@users.noreply.github.com>
2023-04-11 23:35:10 +0530

    Affinity calc fix

--- modules/system-stats/naffinity-calc.mjs
+++ modules/system-stats/naffinity-calc.mjs
@@ -3,6 +3,9 @@ import {
     N_AFFINITY_WEIGHT1,
     N_AFFINITY_WEIGHT2,
     N_AFFINITY_WEIGHT3,
+    N_AFFINITY_WINDOW1,
+    N_AFFINITY_WINDOW2,
+    N_AFFINITY_WINDOW3,
 } from './constants.js';
 
 const getNAffinity = ({ TP, NP, TC, NC, TF, NF, w1, w2, w3 }) => {
@@ -15,13 +18,13 @@ const monitorNAffinity = (timeInterval, callback) => {
     logger('monitor naffinity -- init');
     setInterval(() => {
         //Redefine these variables depending upon what our Time Window will be and how many tasks should be done in that window.
-        const P_TASKS_COUNT = Math.random(); //No. of Tasks completed in past window = TP
-        const C_TASKS_COUNT = Math.random();
-        const F_TASKS_COUNT = Math.random();
+        const P_TASKS_COUNT = Math.random() * 10; //No. of Tasks completed in past window = TP
+        const C_TASKS_COUNT = Math.random() * 10;
+        const F_TASKS_COUNT = Math.random() * 10;
 
-        const P_TIME_WINDOW = Math.random(); //NP; It is the Time window of the past tasks
-        const C_TIME_WINDOW = Math.random();
-        const F_TIME_WINDOW = Math.random();
+        const P_TIME_WINDOW = N_AFFINITY_WINDOW1; //NP; It is the Time window of the past tasks
+        const C_TIME_WINDOW = N_AFFINITY_WINDOW2;
+        const F_TIME_WINDOW = N_AFFINITY_WINDOW3;
 
         callback(
             getNAffinity({
